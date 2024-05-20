07e4b05df6069662ff08bc916ee6d1e6c8e95cf5
Aniket Patil <aniket.p@reapmind.com>
2024-05-20 11:16:21 +0530

    Initial Commit

---
+++ MapLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapLocation
+//
+//  Created by Aniket Patil on 20/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapLocation/MapLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocationApp.swift
+//  MapLocation
+//
+//  Created by Aniket Patil on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MapLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
